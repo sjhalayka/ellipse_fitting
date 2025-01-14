b6a44e00eda97ca225d1394bed5d0bb66023d070
Shawn Halayka <sjhalayka@gmail.com>
2025-01-14 08:33:28 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -438,7 +438,7 @@ VectorXd solveEllipseParameters(const vector<cartesian_point>& points, const vec
 	const double m = mvec[4];
 
 	double d = (mvec[4] - mvec[0]) / mvec[4];
-	d = pow(1 - d, 20.0);
+	d = pow(1 - d, 10.0);
 
 	VectorXd params(4); // h, k, a, b
 	params << 1, 1, m* d, m* d; // Initial guess
@@ -654,7 +654,7 @@ void idle_func(void)
 		{
 			{hours_to_seconds(0),  deg_to_rad(360) + pi / 2},
 			{hours_to_seconds(24), deg_to_rad(359) + pi / 2},
-			{hours_to_seconds(48), deg_to_rad(357.975) + pi / 2}
+			{hours_to_seconds(48), deg_to_rad(357.95) + pi / 2}
 
 			//{hours_to_seconds(0),  deg_to_rad(0) + pi / 2},
 			//{hours_to_seconds(24), deg_to_rad(-1) + pi / 2},
